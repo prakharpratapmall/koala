21fc30fba28c9094419887a03fc87a63868874d8
Siddhartha Pratap Singh <sidps1112@gmail.com>
2023-08-18 16:25:18 +0530

    Add UI with BG Color property

--- app.js
+++ app.js
@@ -1,8 +1,86 @@
-// WebGL Renderer
+//! WEBGL RENDERER
 
+//! Renderer Variables
+//-----------------------------------------------
+var canvas; // Canvas Reference
 var gl; // Renderer reference
 var windowWidth = 500; // Renderer window width
 var windowHeight = 500; // Renderer window height
+var animation; // Reference to the animation element
+//-----------------------------------------------
+
+//! UI Variables
+//-----------------------------------------------
+var sliderR;
+var sliderB;
+var sliderG;
+//-----------------------------------------------
+
+//! Scene Variables
+//-----------------------------------------------
+var bgColorR;
+var bgColorG;
+var bgColorB;
+//-----------------------------------------------
+
+//! Callback Functions
+//-----------------------------------------------
+
+// Resets the Background color
+function resetBGColor() {
+  bgColorR = 0.05;
+  bgColorG = 0.1;
+  bgColorB = 0.15;
+
+  sliderR.querySelector("input").value = bgColorR * 255.0;
+  sliderR.getElementsByClassName("slider-value")[0].innerHTML =
+    Math.round(bgColorR * 100) / 100;
+
+  sliderG.querySelector("input").value = bgColorG * 255.0;
+  sliderG.getElementsByClassName("slider-value")[0].innerHTML =
+    Math.round(bgColorG * 100) / 100;
+
+  sliderB.querySelector("input").value = bgColorB * 255.0;
+  sliderB.getElementsByClassName("slider-value")[0].innerHTML =
+    Math.round(bgColorB * 100) / 100;
+}
+//-----------------------------------------------
+
+//! Renderer Functions
+//-----------------------------------------------
+
+// Initialises the canvas element
+function initCanvas() {
+  canvas = document.getElementById("window");
+  canvas.style.width = windowWidth + "px";
+  canvas.style.height = windowHeight + "px";
+
+  var ui_windows = document.getElementsByClassName("ui-window");
+  var i = 0;
+  while (i < ui_windows.length) {
+    ui_windows[i].style.height = windowHeight + "px";
+    i++;
+  }
+
+  sliderR = document.getElementById("bgRedSlider");
+  sliderR.querySelector("input").oninput = function () {
+    bgColorR = this.value / 255.0;
+    sliderR.getElementsByClassName("slider-value")[0].innerHTML =
+      Math.round(bgColorR * 100) / 100;
+  };
+  sliderG = document.getElementById("bgGreenSlider");
+  sliderG.querySelector("input").oninput = function () {
+    bgColorG = this.value / 255.0;
+    sliderG.getElementsByClassName("slider-value")[0].innerHTML =
+      Math.round(bgColorG * 100) / 100;
+  };
+  sliderB = document.getElementById("bgBlueSlider");
+  sliderB.querySelector("input").oninput = function () {
+    bgColorB = this.value / 255.0;
+    sliderB.getElementsByClassName("slider-value")[0].innerHTML =
+      Math.round(bgColorB * 100) / 100;
+  };
+}
 
 // Initialises WebGL
 function initWebGL(canvas) {
@@ -21,25 +99,38 @@ function initWebGL(canvas) {
 
 // Draws the current scene
 function drawScene() {
-  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight); // Set window size for drawing
+  if (animation) {
+    window.cancelAnimationFrame(animation);
+  }
+  // Render Loop
+  var animate = function () {
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight); // Set window size for drawing
 
-  // Refresh window with background color
-  gl.clearColor(0.7, 0.9, 0.9, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    // Refresh window with background color
+    gl.clearColor(bgColorR, bgColorG, bgColorB, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    animation = window.requestAnimationFrame(animate);
+  };
+  animate();
 }
 
 // Starts the renderer
 function startWebGL() {
   // Get Canvas
-  var canvas = document.getElementById("window");
-  canvas.style.width = windowWidth + "px";
-  canvas.style.height = windowHeight + "px";
+  initCanvas();
 
   // Setup Renderer
   initWebGL(canvas);
 
   // Setup Scene
+  resetBGColor();
+  // Load Shaders
+  // Load Vertex Buffers
+  // Setup Actors
+  // Setup Shader Data
 
   // Draw Scene
   drawScene();
 }
+//-----------------------------------------------
